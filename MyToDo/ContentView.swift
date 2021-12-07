d5ed4b3a2d18ac0e8742de057c1bba784d72fd2d
Pablo <pablo_082006@hotmail.com>
2021-12-07 17:55:18 -0600

    Initial Commit

---
+++ MyToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyToDo
+//
+//  Created by Pablo Grajeda on 7/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyToDo/MyToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoApp.swift
+//  MyToDo
+//
+//  Created by Pablo Grajeda on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
